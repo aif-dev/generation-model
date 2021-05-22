8060db9efaa1b513719fd6fc50104fdc3e39f7fc
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-23 01:24:29 +0200

    Move function to generate notes to file with prediction

--- predict.py
+++ predict.py
@@ -4,12 +4,9 @@ import os
 import sys
 import pickle
 import tensorflow as tf
+import numpy as np
 from network import create_network
-from data_preparation import (
-    save_midi_file,
-    prepare_sequences_for_prediction,
-    generate_notes,
-)
+from data_preparation import save_midi_file, prepare_sequences_for_prediction
 
 
 def get_best_weights_filename():
@@ -29,6 +26,33 @@ def get_best_weights_filename():
 
     print(f"*** Found checkpoint with the best weights: {best_checkpoint} ***")
     return best_checkpoint
+
+
+def generate_notes(model, network_input, pitchnames, n_vocab):
+    """Generate notes from the neural network based on a sequence of notes"""
+    # pick a random sequence from the input as a starting point for the prediction
+    start = np.random.randint(0, len(network_input) - 1)
+
+    int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
+
+    pattern = network_input[start]
+    prediction_output = []
+
+    # generate 500 notes
+    for _ in range(500):
+        prediction_input = np.reshape(pattern, (1, len(pattern), 1))
+        prediction_input = prediction_input / float(n_vocab)
+
+        prediction = model.predict(prediction_input, verbose=0)
+
+        index = np.argmax(prediction)
+        result = int_to_note[index]
+        prediction_output.append(result)
+
+        pattern.append(index)
+        pattern = pattern[1 : len(pattern)]
+
+    return prediction_output
 
 
 def generate_music():
