a9858742daf8553e70a8106a7010c912faab0eec
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-26 18:05:07 +0200

    Add vocabulary creation

--- train.py
+++ train.py
@@ -4,30 +4,41 @@ import tensorflow as tf
 from keras.models import load_model
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from network import create_network
-from data_preparation import get_notes_from_dataset, prepare_sequences_for_training
+from data_preparation import (
+    get_notes_from_dataset,
+    prepare_sequences_for_training,
+    create_vocabulary_for_training,
+)
 
 
 LOG_DIR = "logs/"
 
 
-def train_network():
-    notes = get_notes_from_dataset()
-
-    # get amount of pitch names
-    n_vocab = len(set(notes))
-
-    network_input, network_output = prepare_sequences_for_training(notes, n_vocab)
-
+def get_latest_checkpoint():
     if not os.path.isdir("checkpoints"):
         os.mkdir("checkpoints")
+        return None
 
     checkpoints = ["checkpoints/" + name for name in os.listdir("checkpoints/")]
-    if checkpoints:
-        latest_checkpoint = max(checkpoints, key=os.path.getctime)
+    return max(checkpoints, key=os.path.getctime)
+
+
+def train_network():
+    notes = get_notes_from_dataset()
+    vocab = create_vocabulary_for_training(notes)
+    vocab_size = len(vocab)
+
+    network_input, network_output = prepare_sequences_for_training(
+        notes, vocab, vocab_size
+    )
+
+    latest_checkpoint = get_latest_checkpoint()
+
+    if latest_checkpoint:
         print(f"*** Restoring from the lastest checkpoint: {latest_checkpoint} ***")
         model = load_model(latest_checkpoint)
     else:
-        model = create_network(network_input, n_vocab)
+        model = create_network(vocab_size)
 
     train(model, network_input, network_output)
 
