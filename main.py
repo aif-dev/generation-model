0b803343f31921c93cb51acec49a68c1ce65a848
rachwalk <kajetan.rachwal@mailfence.com>
2021-05-26 10:22:28 +0200

    added file loading

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import pygame
 import components
 
+
 def LoadFileIntoMusicPlayer(listViewer, musicPlayer):
     file = listViewer.GetSelectedFile()
     if file:
         musicPlayer.LoadFile(file.file)
 
+
 def main():
-    #Initializes Pygame Application
+    # Initializes Pygame Application
     pygame.init()
-    #Setting apps dimensions
+    # Setting apps dimensions
     WIDTH = 800
     HEIGHT = 600
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
-    #Setting apps caption
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    # Setting apps caption
     pygame.display.set_caption("JukeBox AI")
-    #Creating title
+    # Creating title
     font = pygame.font.SysFont(None, 40)
-    title = font.render("Welcome to JukeBox AI!", True, (255,255,255))
-    #Title box
+    title = font.render("Welcome to JukeBox AI!", True, (255, 255, 255))
+    # Title box
     title_rect = title.get_rect()
     title_rect.center = (WIDTH // 2, 40)
-    #Initialize generate Button
+    # Initialize generate Button
     genButton = components.GenerateButton(50, 100, 100, 30)
     genButton.SetText("Generate Music")
-    #Initialize ListViewer
-    listViewer = components.ListViewer(WIDTH//2, 100, (WIDTH//2) - 10, HEIGHT//2)
-    mockList = ["Song 1", "Song 2","Song 3"]
-    listViewer.StoreList(mockList)
-    #Initialize Load Button
-    loadButton = components.LoadButton(WIDTH//2, (WIDTH//2 + 7), 100, 30)
+    # Initialize ListViewer
+    listViewer = components.ListViewer(WIDTH // 2, 100, (WIDTH // 2) - 10, HEIGHT // 2)
+    fileList = []
+    listViewer.StoreList(fileList)
+    # Initialize Load Button
+    loadButton = components.LoadButton(WIDTH // 2, (WIDTH // 2 + 7), 100, 30)
     loadButton.SetText("Load File")
-    #Initialize Music player
-    musicPlayer = components.MusicPlayer(0,HEIGHT-150,WIDTH,150)
-    #Initialize refresh list button
-    refreshButton = components.RefreshListButton((WIDTH//4)*3, (WIDTH//2 + 7), 100, 30)
+    # Initialize Music player
+    musicPlayer = components.MusicPlayer(0, HEIGHT - 150, WIDTH, 150)
+    # Initialize refresh list button
+    refreshButton = components.RefreshListButton(
+        (WIDTH // 4) * 3, (WIDTH // 2 + 7), 100, 30
+    )
     refreshButton.SetText("Refresh List")
 
     running = True
     while running:
 
-        screen.fill((32,32,32))
+        screen.fill((32, 32, 32))
         screen.blit(title, title_rect)
         mouse = pygame.mouse.get_pos()
         genButton.AddToScreen(screen, mouse)
@@ -50,17 +54,21 @@ def main():
         refreshButton.AddToScreen(screen, mouse)
 
         for e in pygame.event.get():
-            #Event for shutting own application
+            # Event for shutting own application
             if e.type == pygame.QUIT:
                 pygame.quit()
 
             if e.type == pygame.MOUSEBUTTONDOWN:
                 genButton.MouseDownHandler(mouse)
                 listViewer.MouseDownHandler(mouse)
-                if loadButton.MouseDownHandler(mouse) == True:
-                    LoadFileIntoMusicPlayer(listViewer, musicPlayer)
+                file = loadButton.MouseDownHandler(mouse)
+                if file is not False:
+                    fileList.append(file)
+                    listViewer.StoreList(fileList)
+                    # LoadFileIntoMusicPlayer(listViewer, musicPlayer)
                 musicPlayer.MouseDownHandler(mouse)
 
         pygame.display.flip()
+
 
 main()
