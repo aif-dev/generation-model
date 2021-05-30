86cdf9e376252e5083642095507361a6688ed1ae
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-30 21:15:53 +0200

    Add matrix sequences in generator

--- notes_sequence.py
+++ notes_sequence.py
@@ -4,21 +4,11 @@ import numpy as np
 
 
 class NotesSequence(Sequence):
-    def __init__(
-        self,
-        notes,
-        batch_size,
-        sequence_length,
-        vocab,
-        vocab_size,
-        num_notes_to_predict,
-    ):
+    def __init__(self, notes, batch_size, sequence_length, prediction_size):
         self.notes = notes
         self.batch_size = batch_size
         self.sequence_length = sequence_length
-        self.vocab = vocab
-        self.vocab_size = vocab_size
-        self.num_notes_to_predict = num_notes_to_predict
+        self.prediction_size = prediction_size
 
     def __len__(self):
         return math.floor(len(self.notes[: -self.sequence_length]) / self.batch_size)
@@ -35,16 +25,13 @@ class NotesSequence(Sequence):
             ]
             note_out = self.notes[idx * self.batch_size + i + self.sequence_length]
 
-            network_input.append([self.vocab[note] for note in sequence_in])
-            network_output.append(self.vocab[note_out])
+            network_input.append([note for note in sequence_in])
+            network_output.append(note_out)
 
         n_patterns = len(network_input)
 
-        unnormalized_network_input = np.reshape(
-            network_input, (n_patterns, self.sequence_length, self.num_notes_to_predict)
+        network_input = np.reshape(
+            network_input, (n_patterns, self.sequence_length, self.prediction_size)
         )
-        normalized_network_input = unnormalized_network_input / float(self.vocab_size)
 
-        network_output = np_utils.to_categorical(network_output, self.vocab_size)
-
-        return normalized_network_input, network_output
+        return network_input, network_output
