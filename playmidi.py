f73868ae4802d83b60a0172b118b182038450ebd
MatthewChapman99 <mattdch99@outlook.com>
2021-06-02 10:10:21 +0200

    Added midi playing functionality (needs connecting)

---
+++ playmidi.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+def play_music(music_file):
+    clock = pygame.time.Clock()
+    try:
+        pygame.mixer.music.load(music_file)
+        print "Music file %s loaded!" % music_file
+    except pygame.error:
+        print "File %s not found! (%s)" % (music_file, pygame.get_error())
+        return
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        # check if playback has finished
+        clock.tick(30)
+
+
+def init_player():
+    freq = 44100
+    bitsize = -16
+    channels = 2
+    buffer = 1024
+    pygame.mixer.init(freq, bitsize, channels, buffer)
+    pygame.mixer.music.set_volume(0.8)
